8d1d57350b8901052f8b88d1396fe482ad81e073
Nanoteck137 <patrik.millvik@gmail.com>
2018-07-04 22:02:59 +0200

    Converted the project from c++ to c# because i wanted to.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] files = Directory.GetFiles("obj/");
+        foreach(string file in files)
+        {
+            string newPath = Path.GetFullPath(file);
+            Console.WriteLine("File: " + newPath);
+        }
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("This is a test for fun!");
+
+        Console.ReadLine();
+    }
+}
