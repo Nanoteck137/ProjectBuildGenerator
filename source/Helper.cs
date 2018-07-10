8de96f64b43954ec981f97651633d55625452c7a
Nanoteck137 <patrik.millvik@gmail.com>
2018-07-10 18:12:59 +0200

    Added better error handling in main

--- source/Helper.cs
+++ source/Helper.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.IO;
 using System;
 
@@ -9,4 +10,27 @@ public class Helper
     {
         return Directory.GetFiles(dirPath, ext, SearchOption.AllDirectories);
     }
+
+    public static void ErrorExit(string prefix, string message)
+    {
+        string errorMessage = string.Format("{0} {1} error: {2}", AppDomain.CurrentDomain.FriendlyName, prefix, message);
+        Console.WriteLine(errorMessage);
+        Debug.Assert(false, errorMessage);
+        Environment.Exit(-1);
+    }
+
+    public static void ErrorExit(string message)
+    {
+        string errorMessage = string.Format("{0} error: {1}", AppDomain.CurrentDomain.FriendlyName, message);
+        Console.WriteLine(errorMessage);
+        Debug.Assert(false, errorMessage);
+        Environment.Exit(-1);
+    }
+
+    public static void Exit()
+    {
+        Debug.Assert(false);
+        Environment.Exit(-1);
+    }
+
 }
--- source/Main.cs
+++ source/Main.cs
@@ -1,195 +1,35 @@
-﻿using System.Collections.Generic;
-using System.Text;
-using System.Linq;
+﻿using System.Runtime.InteropServices;
+using System.Diagnostics;
 using System.IO;
 using System;
 
 using MoonSharp.Interpreter;
 
-/*
-    TODO:
-     - Need to redo the code generator for make files
-     - Need to implement the code generator 
-        for batch and shell files
-     - Give all the object compile result a diffrent name like: ProjectName_InputName.obj
- */
-
-enum Mode
-{
-    Unknown,
-    Windows,
-    Linux,
-}
-
-/*class Program
-{
-    public Program(Mode mode)
-    {
-        foreach(Project.Project project in projects)
-        {
-            CreateMakeTargetsFromProject(makeGenerator, project, config);
-        }
-
-        string makeCode = makeGenerator.GenCode();
-        File.WriteAllText("Makefile.gen.win32", makeCode);
-
-        string buildDir = Path.Combine(Directory.GetCurrentDirectory(), "build");
-        string makeFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Makefile.gen.win32");
-
-        ProjectBatch batch = new ProjectBatch(projects);
-        string batchCode = batch.CreateBatchCode(buildDir, makeFilePath);
-        File.WriteAllText("build.gen.bat", batchCode);
-
-        Console.WriteLine(makeCode);
-        Console.WriteLine();
-        Console.WriteLine();
-        Console.WriteLine();
-        Console.WriteLine(batchCode);
-    }
-
-/*    private void CreateMakeTargetsFromProject(MakeGen.Generator generator, Project.Project project, Config config)
-    {
-        List<string> objFiles = new List<string>();
-        foreach(string file in project.Files)
-        {
-            string targetName = project.Name + "_" + Helper.GetObjFileName(config, file);
-            objFiles.Add(targetName);
-
-            MakeGen.Target targetRes = new MakeGen.Target(); 
-            targetRes.Name = targetName;
-            targetRes.Dependencies = new string[] { file };
-            targetRes.Commands = new MakeGen.Command[] {
-                new MakeGen.Command(config.Compiler, new string[] { config.CompilerOnlySwitch, file, config.OutputObjSwitch, targetName})
-            };
-
-            generator.AddTarget(targetRes);
-        }
-
-        switch(project.Type)
-        {
-            case Project.Type.Executable: {
-                generator.AddTarget(CreateTargetForExecutable(objFiles, generator, project, config));
-            } break;
-
-            case Project.Type.StaticLibrary:
-                generator.AddTarget(CreateTargetForStaticLibrary(objFiles, generator, project, config));
-                break;
-            case Project.Type.DynamicLibrary:
-                break;
-
-            default: throw new Exception();
-        }
-    }
-
-    private MakeGen.Target CreateTargetForStaticLibrary(List<string> objFiles, MakeGen.Generator generator, Project.Project project, Config config)
-    {
-        MakeGen.Target target = new MakeGen.Target();
-
-        string name = project.Name + Helper.GetStaticLibraryFileExt(config.Mode);
-
-        List<string> arguments = new List<string>();
-        arguments.Add("/out:" + name);
-        arguments.AddRange(objFiles);
-
-        target.Name = name;
-        target.Dependencies = objFiles.ToArray();
-        target.Commands = new MakeGen.Command[] {
-            new MakeGen.Command("lib", arguments.ToArray())
-        };
-
-        return target;
-    }
-
-    private MakeGen.Target CreateTargetForExecutable(List<string> objFiles, MakeGen.Generator generator, Project.Project project, Config config)
-    {
-        MakeGen.Target target = new MakeGen.Target();
-
-        string name = project.Name + Helper.GetExecutableFileExt(config.Mode);
-
-        List<string> arguments = new List<string>();
-
-        if (project.ProjectDependencies != null)
-        {
-            foreach (string dep in project.ProjectDependencies)
-            {
-                arguments.Add(Helper.GetDependencyFileName(dep, config.Mode));
-            }
-        }
-
-        arguments.AddRange(objFiles);
-        arguments.Add(config.OutputExeSwitch);
-        arguments.Add(name);
-
-        if(project.ProjectDependencies != null)
-        {
-            foreach(string dep in project.ProjectDependencies)
-            {
-                objFiles.Add(Helper.GetDependencyFilePrefix(config.Mode) + dep + Helper.GetDependencyFileExt(config.Mode));
-            }
-        }
-
-        target.Name = name;
-        target.Dependencies = objFiles.ToArray();
-        target.Commands = new MakeGen.Command[] {
-            new MakeGen.Command(config.Compiler, arguments.ToArray())
-        };
-
-        return target;
-    }
-
-    public static void Main(string[] args)
-    {
-        //TODO: Redesign how the command arguments are passed
-        Mode mode = Mode.Unknown;
-        if(args.Length == 1)
-        {
-            if(args[0] == "-win32")
-                mode = Mode.Windows;
-            else if(args[0] == "-linux")
-                mode = Mode.Linux;
-            else
-                throw new Exception();
-        }
-
-
-        new Program(mode);
-    }
-}*/
-
 public class MainClass
 {
-    /*
-        Structure of the Command line arguments
-        ProjectBuildGenerator.exe
-        [options]: --windows, --linux and other options
-                 : --buildDirPath=path - This is the path to where the build files is gonna live, this can be a absolute path or relative.
-                 : --workspaceDirPath=path - This is the path where lua's Working directory is, 
-                    this can be a absolute path or relative 
-                    this is gonna get resolved later with Path.GetFullPath().
-        luaFilePath: C:\CSharp\ProjectBuildGenerator\Test2\test.lua
-     */
-
-    public static void PrintUsage()
+    private static void PrintUsage()
     {
         Console.WriteLine("Usage: {0} [options] LuaFilePath", AppDomain.CurrentDomain.FriendlyName);
         Console.WriteLine("Options:");
-        Console.WriteLine("  --windows              - Switches the windows code generator on");
-        Console.WriteLine("  --linux                - Switches the linux code generator on");
-        Console.WriteLine("  --buildDirPath:        - Sets where the build directory should be");
+        Console.WriteLine("  --windows              - (Default on Windows) Switches the windows code generator on");
+        Console.WriteLine("  --linux                - (Default on Linux) Switches the linux code generator on");
         Console.WriteLine("  --workspaceDirPath     - Sets where lua should start looking for projects");
     }
 
     public static void Main(string[] commandLine)
     {
         string luaFilePath = "";
-        string buildDirPath = "";
         string workspaceDirPath = "";
 
         bool IsWindows = false;
         bool IsLinux = false;
 
         if(commandLine.Length <= 0)
-            throw new Exception();
+        {
+            Console.WriteLine("You need to specify the lua file path");
+            PrintUsage();
+            Helper.Exit();
+        }
 
         for(int index = 0; 
             index < commandLine.Length; 
@@ -203,7 +43,7 @@ public class MainClass
                 if(File.Exists(filePath))
                     luaFilePath = filePath;
                 else
-                    throw new Exception();
+                    Helper.ErrorExit("The Last argument needs to be a path to the lua file");
 
                 break;
             }
@@ -228,13 +68,6 @@ public class MainClass
                         IsLinux = true;
                         break;
 
-                    case "builddirpath":
-                        if(splittedArgs.Length == 2)
-                            buildDirPath = Path.GetFullPath(splittedArgs[1]);
-                        else
-                            throw new Exception();
-                        break;
-
                     case "workspacedirpath":
                         if (splittedArgs.Length == 2)
                             workspaceDirPath = Path.GetFullPath(splittedArgs[1]);
@@ -250,21 +83,19 @@ public class MainClass
             }
             else
             {
-                Console.WriteLine("Unknown argument: {0}", arg);
-                PrintUsage();
-                Environment.Exit(-1);
+                Helper.ErrorExit(string.Format("Unknown argument: {0}", arg));
             }
         }
 
-        if(luaFilePath == "")
-            throw new Exception();
-        if(buildDirPath == "")
-            throw new Exception();
         if(workspaceDirPath == "")
-            throw new Exception();
+            workspaceDirPath = Directory.GetCurrentDirectory();
 
-        if(!IsWindows && !IsLinux)
-            throw new Exception();
+        if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            IsWindows = true;
+        else if(RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            IsLinux = true;
+        else
+            Helper.ErrorExit("Windows and Linux are the only platform supported");
 
         UserData.RegisterAssembly(System.Reflection.Assembly.GetExecutingAssembly());
 
